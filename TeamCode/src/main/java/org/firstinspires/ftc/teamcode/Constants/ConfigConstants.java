cb32670feeb73ff602a5a9a7dba4c8750408bf34
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2025-08-31 18:11:51 -0700

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants/ConfigConstants.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.Constants;
+
+public class ConfigConstants {
+    public static String FrontLeftMotorConfig = "motor_fl";
+    public static String FrontRightMotorConfig = "motor_fr";
+    public static String BackLeftMotorConfig = "motor_bl";
+    public static String BackRightMotorConfig = "motor_br";
+}
